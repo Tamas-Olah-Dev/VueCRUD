1da6af4eeaf196ec33c4e0feec6bc0c0fa6cd18b
OlahTamas <olah.tamas@datalytix.tech>
2020-08-05 12:30:59 +0000

    Added image library

---
+++ src/Formdatabuilders/Formfieldtypes/ImageLibraryVueCRUDFormfield.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Datalytix\VueCRUD\Formdatabuilders\Formfieldtypes;
+
+
+class ImageLibraryVueCRUDFormfield extends VueCRUDFormfield
+{
+    const ACCEPTS_PRESET_PDF = '.pdf,application/pdf';
+    const ACCEPTS_PRESET_IMAGE = '.jpg,.jpeg,.png,.gif,image/jpeg,image/gif,image/png';
+    /**
+     * FileCollectorVueCRUDFormfield constructor.
+     * @param array $properties
+     */
+    public function __construct($properties = [])
+    {
+        parent::__construct($properties);
+        $this->kind = 'custom-component';
+        $this->type = 'image-library';
+        $this->props['subComponentValuesetProp'] = 'originalOptions';
+        $this->addAcceptCondition(self::ACCEPTS_PRESET_IMAGE);
+    }
+
+    public function addRoutes($slug)
+    {
+        $id = \Route::getCurrentRoute()->hasParameter('subject')
+            ? \Route::getCurrentRoute()->parameters()['subject']
+            : -1;
+        $this->props['uploadUrl'] = route('vuecrud_'.$slug.'_ajax_operations', ['subject' => $id]);
+
+        return $this;
+    }
+
+    public function setLimit($limit)
+    {
+        $this->props['limit'] = $limit;
+
+        return $this;
+    }
+
+    /**
+     * @param $conditionString
+     * https://w3c.github.io/html/sec-forms.html#element-attrdef-input-accept
+     * @return $this
+     */
+    public function addAcceptCondition($conditionString)
+    {
+        if (!isset($this->props['accept'])) {
+            $this->props['accept'] = [];
+        }
+        $this->props['accept'][] = $conditionString;
+
+        return $this;
+    }
+}
