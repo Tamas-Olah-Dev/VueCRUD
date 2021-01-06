067d80f0195ab6862fd8f98d9a282652ec4bab6d
OlahTamas <olah.tamas@datalytix.tech>
2021-01-06 09:45:53 +0000

    Added backlink to view

--- src/resources/views/model-manager.blade.php
+++ src/resources/views/model-manager.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
 @section('content')
+    @if((isset($backlink)) && ($backlink != null))
+        <a style="font-weight: bold; color: blue; text-decoration: underline" href="{{ $backlink['url'] }}">{{ $backlink['label'] }}</a>
+    @endif
+
     @include('packages.vue-crud.model-manager-inner')
 @endsection
